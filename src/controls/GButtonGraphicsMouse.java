0e41e4bafa1b3f31292023ce06dfee052e0c2e9f
bjoukovs <borisjoukov@hotmail.com>
2016-04-22 16:21:01 +0200

    (il manquait des fichiers au commit)

---
+++ src/controls/GButtonGraphicsMouse.java
@@ -0,0 +1,41 @@
+package controls;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import view.button.GButtonGraphics;
+
+public class GButtonGraphicsMouse implements MouseListener {
+	
+	private GButtonGraphics button;
+	
+	public GButtonGraphicsMouse(GButtonGraphics b) {
+		button = b;
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		button.setGraphics("over");
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		button.setGraphics("out");
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		button.setGraphics("down");
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		button.setGraphics("over");
+	}
+
+}
